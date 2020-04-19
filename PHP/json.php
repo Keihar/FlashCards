adc56445614f3e2c63ada5e1b7074c6420baac7d
alegordan <agorla10@gmail.com>
2020-04-19 15:21:10 +0100

    json.php commentato

--- PHP/json.php
+++ PHP/json.php
@@ -1,27 +1,31 @@
 <?php
 
+    //faccio partire la sessione
     session_start();
 
+    //includo codice per connessione al DataBase
     include 'DBconnect.php';
 
+    //attribuisco a $user la variabile di sessione che identifica l'utente
     $user = $_SESSION['utente'];
 
+    //query per prelevamento dei campi dell'album
     $sql = "SELECT id, Album.nome, descrizione FROM Album INNER JOIN Utente ON (utente.email = Album.email) WHERE Utente.nome = '$user'";
     $result = mysqli_query($connect, $sql);
 
-    if(!$result){
-        die('Invalid query: ');
-    }
-
+    //istanzio array che mi servirà per splittare le righe
     $json_array = array();
 
+    //splitto le righe che ottengo dalla query
     while($row = mysqli_fetch_assoc($result))
     {
         $json_array[] = $row;
     }
 
+    //istanzio l'array per creare la matrice
     $albums = array();
 
+    //ciclo che assegna per ogni array i campi
     foreach($json_array as &$rows){
         array_push($albums, array(
             'id' => $rows['id'],
@@ -30,11 +34,13 @@
         ));
     }
 
+    //array di array
     $post_data = array(
         'nome' => $_SESSION['utente'],
         'albums' => $albums
     );
 
+    //codifica in json
     $output = json_encode($post_data);
 
     echo $output;
