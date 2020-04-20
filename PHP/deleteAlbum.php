2ef18edbd38e512548643d82c9c03bb4a1874059
alegordan <agorla10@gmail.com>
2020-04-20 18:56:32 +0100

    PHP deleteAlbum

---
+++ PHP/deleteAlbum.php
@@ -0,0 +1,24 @@
+<?php
+
+        //includo codice per la connessione al DataBase
+        include 'DBconnect.php';
+
+        //inizializzo la sessione
+        session_start();
+    
+        //catcho la variabile di sessione
+        $id_album = $_SESSION['currentAlbum'];
+
+        //query per cancellare il suddetto album
+        $delete_album_str = "DELETE FROM Album WHERE id=".$id_album."";
+        $delete_album = mysqli_query($connect, $delete_album_str);
+
+        //controllo se la query è andata a buon fine per utilizzare l'ajax
+        if(!$delete_album){
+            echo "error";
+        }
+        else{
+            echo "success";
+        }
+
+?>
