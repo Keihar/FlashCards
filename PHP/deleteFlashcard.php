41586b58600e5a0862b5420ca368e84e95cd937f
alegordan <agorla10@gmail.com>
2020-04-20 19:08:47 +0100

    PHP deleteFlashcard

---
+++ PHP/deleteFlashcard.php
@@ -0,0 +1,25 @@
+<?php
+
+    //includo codice per la connessione al DataBase
+    include 'DBconnect.php';
+
+    //inizializzo la sessione
+    session_start();
+    
+    //prendo l'ID della flashcard da cancellare
+    $id_flashcard = $_POST["id"];
+
+    //query per cancellare la suddetta flashcard
+    $delete_flashcard_str = "DELETE FROM Flashcard WHERE id=".$id_flashcard."";
+    $delete_flashcard = mysqli_query($connect, $delete_flashcard_str);
+
+    //controllo se la query è andata a buon fine per utilizzare l'ajax
+    if(!$delete_flashcard){
+        echo "error";
+    }
+    else{
+        echo "success";
+    }
+
+
+?>
