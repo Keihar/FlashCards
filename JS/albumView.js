6e840b3a26b94ef954cd65abcb3a4e89aaee416d
Keihar <lorenz.com@hotmail.it>
2020-04-22 10:57:38 +0200

    Temporary modidyRow

--- JS/albumView.js
+++ JS/albumView.js
@@ -14,6 +14,7 @@ $.ajax({
       tableBody.innerHTML += "<tr>" +
         `<td>${flashcard.fronte}</td>` +
         `<td>${flashcard.retro}</td>` +
+        `<td> <button type="button" onclick="modifyRow(this, ${flashcard.id})" class="btn btn-outline-secondary btn-sm">Modifica</button></td>` +
         `<td> <button type="button" onclick="deleteRow(this, ${flashcard.id})" class="btn btn-outline-danger btn-sm">Rimuovi</button></td>` +
         "</tr>";
     });
@@ -33,6 +34,21 @@ function deleteRow(btn, id) {
       }
     }
   });
+}
+
+function modifyRow(btn, id) {
+  let row = $(btn).parent().parent()[0];
+  let cols = $(row).children();
+  // Fronte
+  cols[0].innerHTML = `<div class="input-group input-group-sm"><div class="input-group-prepend"><span class="input-group-text" id="inputGroup-sizing-sm">Nuovo Fronte</span></div><input type="text" value="${cols[0].innerHTML}" class="form-control w-3" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></div>`;
+  // Retro
+  cols[1].innerHTML = `<div class="input-group input-group-sm"><div class="input-group-prepend"><span class="input-group-text" id="inputGroup-sizing-sm">Nuovo Retro</span></div><input type="text" value="${cols[1].innerHTML}" class="form-control w-3" aria-label="Small" aria-describedby="inputGroup-sizing-sm"></div>`;
+  // Save
+  cols[2].innerHTML = `<button class="btn btn-secondary btn-sm w" onclick="saveNewRow(${id})"><i class="far fa-save"></i> Salva</button>`;
+}
+
+function saveNewRow(id) {
+  
 }
 
 $("#flashcardsForm").submit(function (e) {
@@ -56,6 +72,7 @@ $("#flashcardsForm").submit(function (e) {
         let row = "<tr>" +
           `<td>${front.value}</td>` +
           `<td>${back.value}</td>` +
+          `<td> <button type="button" onclick="modifyRow(this, ${id})" class="btn btn-outline-secondary btn-sm">Modifica</button></td>` +
           `<td> <button type="button" onclick="deleteRow(this, ${id})" class="btn btn-outline-danger btn-sm">Rimuovi</button></td>` +
           "</tr>";
         front.value = "";
