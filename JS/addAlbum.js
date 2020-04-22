9ede00103fdd2c195352667045e6c6e3bb3a531a
Keihar <lorenz.com@hotmail.it>
2020-04-22 15:11:54 +0200

    Aggiunte icone

--- JS/addAlbum.js
+++ JS/addAlbum.js
@@ -1,4 +1,5 @@
 $("#addAlbum").submit(function (e) {
+    document.getElementById("imgLink").value = document.getElementById('currentIcon').src;
     e.preventDefault();
     var form = $(this);
     var url = form.attr('action');
@@ -18,3 +19,21 @@ $("#addAlbum").submit(function (e) {
       }
     });
   });
+
+var dir = "images/albumCovers";
+var fileextension = ".svg";
+$.ajax({
+    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+    url: dir,
+    success: function (data) {
+        //List all .png file names in the page
+        $(data).find("a:contains(" + fileextension + ")").each(function () {
+            var filename = this.href.replace(window.location.host, "").replace("http://", "");
+            $("#icons").append(`<img class="img-thumbnail imgList" data-dismiss="modal" src="${dir}${filename}" onclick="changeIcon('${filename}');">`);
+        });
+    }
+});
+
+function changeIcon(filename) {
+  document.getElementById('currentIcon').src = dir + filename;
+}
