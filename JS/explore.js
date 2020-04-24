322d65cc6562689d7ae3fe1f917ef098dea2ba1e
Keihar <lorenz.com@hotmail.it>
2020-04-24 14:29:26 +0200

    documented explore.js

--- JS/explore.js
+++ JS/explore.js
@@ -1,84 +1,101 @@
+//  Variable that memorizes the JSON avaible for all functions
 var user;
 
+// Search
 function search() {
-    var query = document.getElementById("searchBar").value;
-    if (query == "") {
-        return;
-    }
-    let row = document.getElementById("row");
-    row.innerHTML = "<div class='mb-3 col'> Caricamento...</div>";
-    $.ajax({
-        type: "POST",
-        url: "PHP/searchQuery.php",
-        data: { 'query': query },
-        success: function (data) {
-            try {
-                console.log(data);
-                user = JSON.parse(data);
-                if (user.albums.length == 0) {
-                  row.innerHTML = "<div class='mb-3 col'> Nessun risultato.</div>"
-                }
-                else {
-                  row.innerHTML = "";
-                }
-                user.albums.forEach(album => {
-                  if (album.imgLink == null) {
-                    album.imgLink = "images\\albumCovers\\000-icon.svg";
-                  }
-                  row.innerHTML += "" + getCard(album.id, album.nome, album.descrizione,
-                    album.imgLink, album.nomeutente);
-                });
-            } 
-            catch (error) {
-                console.log("Query search failed");
-            }
+  var query = document.getElementById("searchBar").value;
+  // If the user doesn't type something returns
+  if (query == "") {
+    return;
+  }
+  //  Charging text
+  let row = document.getElementById("row");
+  row.innerHTML = "<div class='mb-3 col'> Caricamento...</div>";
+  //  Ajax request
+  $.ajax({
+    type: "POST",
+    url: "PHP/searchQuery.php",
+    data: { 'query': query },
+    success: function (data) {
+      try {
+        user = JSON.parse(data);
+        //  Zero results
+        if (user.albums.length == 0) {
+          row.innerHTML = "<div class='mb-3 col'> Nessun risultato.</div>"
         }
-      });
-}
-
-function getCard(id, name, description, imgLink, author) {
-
-    var descMaxLength = 32;
-    if (description.length > descMaxLength) {
-      description = description.substring(0, descMaxLength) + "...";
-    }
-    
-    let str = `<div class="card mb-3 ml-3" style="width: 45%"> <div class="row no-gutters"> <div class="col-md-4">`+
-    `<img src="${imgLink}"> </div> <div class="col-md-8"> <div class="card-body"> <h5 class="card-title">${name}</h5> `+
-    `<p class="card-text">${description}</p><p class="card-text">`+
-    `<a href='#' onclick='playAlbum(${id})' class='btn btn-primary'> <i class="fa fa-plus"></i> Aggiungi ai tuoi Albums</a>` +
-    `<button data-toggle="modal" data-target="#exampleModal" onclick='albumPreview(${id})' class='btn btn-secondary ml-1'><i class="fa fa-pencil"></i> Anteprima</button>` +
-    `</p> <p class="card-text"><a href="#" class="text-secondary" onclick="setCurrentUser(${author})">Creato da ${author}</a></p> </div> </div> </div> </div>`;
-    return str;
-}
-
-function albumPreview(id) {
-  user.albums.forEach(album => {
-    if (album.id == id) {
-      if (album.imgLink == null) {
-        album.imgLink = "images\\albumCovers\\000-icon.svg";
+        else {
+          row.innerHTML = "";
+        }
+        //  Print every album
+        console.table(user);
+        user.albums.forEach(album => {
+          if (album.imgLink == null) {
+            album.imgLink = "images\\albumCovers\\000-icon.svg";
+          }
+          row.innerHTML += "" + getCard(album.id, album.nome, album.descrizione,
+            album.imgLink, album.nomeutente);
+        });
       }
-      document.getElementById("modal-title").innerHTML = "" + album.nome;
-      document.getElementById("modal-body").innerHTML = "" + album.descrizione;
-      let tableBody = document.getElementById("tableBody");
-      tableBody.innerHTML = "";
-      for (let i = 0; i < 10; i++) {
-        tableBody.innerHTML += "<tr>" +
-        `<td>${"flashcard.fronte".replace('\\','')}</td>` +
-        `<td>${"flashcard.retro".replace('\\','')}</td>` +
-        "</tr>";
+      //  Prints the eventual errors 
+      catch (error) {
+        console.error(data);
+        row.innerHTML = "<div class='mb-3 col'> Errore nella ricerca.</div>"
       }
     }
   });
 }
 
-$(function() {
+//  Card formatter
+function getCard(id, name, description, imgLink, author) {
+
+  //  Truncate strings that exceeds the max length
+  var descMaxLength = 32;
+  if (description.length > descMaxLength) {
+    description = description.substring(0, descMaxLength) + "...";
+  }
+
+  //  Returns the formatted HTML
+  return `<div class="card mb-3 ml-3" style="width: 45%"> <div class="row no-gutters"> <div class="col-md-4">` +
+    `<img src="${imgLink}"> </div> <div class="col-md-8"> <div class="card-body"> <h5 class="card-title">${name}</h5> ` +
+    `<p class="card-text">${description}</p><p class="card-text">` +
+    `<a href='#' onclick='playAlbum(${id})' class='btn btn-primary'> <i class="fa fa-plus"></i> Aggiungi ai tuoi Albums</a>` +
+    `<button data-toggle="modal" data-target="#exampleModal" onclick='albumPreview(${id})' class='btn btn-secondary ml-1'><i class="fa fa-pencil"></i> Anteprima</button>` +
+    `</p> <p class="card-text"><a href="#" class="text-secondary" onclick="setCurrentUser(${author})">Creato da ${author}</a></p> </div> </div> </div> </div>`;
+}
+
+//  Album Preview
+function albumPreview(id) {
+  user.albums.forEach(album => {
+    if (album.id == id) {
+      //  Setting default image if its's null
+      album.imgLink = album.imgLink == null ? "images\\albumCovers\\000-icon.svg": album.imgLink;
+
+      //  Setting title and description
+      document.getElementById("modal-title").innerHTML = "" + album.nome;
+      document.getElementById("modal-body").innerHTML = "" + album.descrizione;
+
+      //  Adds backs and fronts to the table
+      let tableBody = document.getElementById("tableBody");
+      tableBody.innerHTML = "";
+
+      album.flashcards.forEach(flashcard => {
+        tableBody.innerHTML += "<tr>" +
+          `<td>${flashcard.fronte.replace('\\', '')}</td>` +
+          `<td>${flashcard.retro.replace('\\', '')}</td>` +
+          "</tr>";
+      });
+    }
+  });
+}
+
+//  Enter shortcut for search bar
+$(function () {
   $("#searchBar").keypress(function (e) {
-      if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-          $('button[type=search]').click();
-          return false;
-      } else {
-          return true;
-      }
+    if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+      $('button[type=search]').click();
+      return false;
+    } else {
+      return true;
+    }
   });
 });
