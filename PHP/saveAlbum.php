7cbedffa9fef3e353f6b84ec73b047f1481cd4e6
alegordan <agorla10@gmail.com>
2020-04-26 13:53:35 +0100

    PHP saveAlbum con check se l'album è già stato salvato

--- PHP/saveAlbum.php
+++ PHP/saveAlbum.php
@@ -9,10 +9,14 @@
     $id_album = mysqli_real_escape_string($connect, $_POST["id_album"]);
     $email = mysqli_real_escape_string($connect, $_POST["email"]);
     
-    $result_str = "SELECT * FROM album_salvati WHERE emailUtente LIKE '$email' AND idAlbum = '$id_album')";
+    $result_str = "SELECT * FROM album_salvati WHERE emailUtente = '$email' AND idAlbum = '$id_album')";
     $result = mysqli_query($connect, $result_str);
 
-    if (!$result) { exit("saveExisting"); }
+    while ($row = mysqli_fetch_assoc($result)) {
+        $array[] = $row;
+    }
+
+    if (mysqli_num_rows($array)==0) { exit("saveExisting"); }
 
     $insert_saved_str = "INSERT INTO album_salvati(emailUtente, idAlbum) VALUES ('$email', '$id_album')";
     $insert_saved = mysqli_query($connect, $insert_saved_str);
