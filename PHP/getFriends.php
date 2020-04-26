10966f0737dd027822aac19a535cefedf74b4d79
alegordan <agorla10@gmail.com>
2020-04-26 18:44:25 +0100

    PHP getFriends

---
+++ PHP/getFriends.php
@@ -0,0 +1,132 @@
+<?php
+
+    //mi passa nome utente e devo ritornare un json contenente tre array: 
+    //1. Amici (mutuals)
+    //2. Seguaci
+    //3. Seguiti
+    //Passo una variabile Namici, Nseguaci, Nseguiti
+    //Di ogni amico e seguace e seguito devo passare username e il link dell'immagine di profilo
+
+    //includo codice per la connessione al DataBase
+    include 'DBconnect.php';
+
+    //inizializzo la sessione
+    session_start();
+
+    //$username = mysqli_escape_string($connect, $_POST["username"]);
+
+    $username = "Alessandro";
+
+    //prendo email utente
+    $get_email_str = "SELECT email FROM follow INNER JOIN Utente ON (follow.seguente = Utente.email) WHERE Utente.nome = '$username'";
+    $get_email = mysqli_query($connect, $get_email_str);
+
+    while($row = mysqli_fetch_array($get_email)){
+        $email = mysqli_real_escape_string($connect, $row["email"]);
+    }
+
+    //prendo amici
+    $checkFollowing_query = "SELECT u.nome AS nome_seguito, u.imgProfilo AS profilo_seguito FROM Utente INNER JOIN follow f ON (f.seguente=Utente.email) INNER JOIN Utente u ON (u.email=f.seguito) WHERE Utente.email='$email'";
+    $checkFollowing = mysqli_query($connect, $checkFollowing_query);
+
+    $conta_seguiti_query = "SELECT COUNT(*) AS nSeguiti FROM Utente INNER JOIN follow f ON (f.seguente=Utente.email) INNER JOIN Utente u ON (u.email=f.seguito) WHERE Utente.email='$email'";
+    $conta_seguiti = mysqli_query($connect, $conta_seguiti_query);
+
+    while($row = mysqli_fetch_array($conta_seguiti)){
+        $nSeguiti = mysqli_real_escape_string($connect, $row["nSeguiti"]);
+    }
+
+    //istanzio array che mi servirà per splittare le righe
+    $json_array_seguiti = array();
+
+    //splitto le righe che ottengo dalla query
+    while ($row = mysqli_fetch_assoc($checkFollowing)) {
+        $json_array_seguiti[] = $row;
+    }
+
+        //istanzio l'array per creare la matrice
+    $seguiti = array();
+
+        //ciclo che assegna per ogni array i campi
+        foreach ($json_array_seguiti as &$rows) {
+            array_push($seguiti, array(
+                'nome_seguito' => $rows["nome_seguito"],
+                'profilo_seguito' => $rows["profilo_seguito"]
+            ));
+        }
+
+    $checkFollowers_query = "SELECT u.nome AS nome_seguente, u.imgProfilo AS profilo_seguente FROM Utente INNER JOIN follow f ON (f.seguito=Utente.email) INNER JOIN Utente u ON (u.email=f.seguente) WHERE Utente.email='$email'";
+    $checkFollowers = mysqli_query($connect, $checkFollowers_query);
+
+    $conta_seguaci_query = "SELECT COUNT(*) AS nSeguaci FROM Utente INNER JOIN follow f ON (f.seguito=Utente.email) INNER JOIN Utente u ON (u.email=f.seguente) WHERE Utente.email='$email'";
+    $conta_seguaci = mysqli_query($connect, $conta_seguaci_query);
+
+    while($row = mysqli_fetch_array($conta_seguaci)){
+        $nSeguaci = mysqli_real_escape_string($connect, $row["nSeguaci"]);
+    }   
+
+    //istanzio array che mi servirà per splittare le righe
+    $json_array_seguaci = array();
+
+    //splitto le righe che ottengo dalla query
+    while ($row = mysqli_fetch_assoc($checkFollowers)) {
+        $json_array_seguaci[] = $row;
+    }
+
+        //istanzio l'array per creare la matrice
+    $seguaci = array();
+
+        //ciclo che assegna per ogni array i campi
+        foreach ($json_array_seguaci as &$rows) {
+            array_push($seguaci, array(
+                'nome_seguace' => $rows["nome_seguente"],
+                'profilo_seguace' => $rows["profilo_seguente"]
+            ));
+        }
+
+    $checkFriends_query = "SELECT uf.nome AS nome_amico, uf.imgProfilo AS profilo_amico FROM Utente u INNER JOIN follow f ON (f.seguente=u.email) INNER JOIN follow ff ON (ff.seguito,ff.seguente) = (f.seguente,f.seguito) INNER JOIN Utente uf ON (uf.email = f.seguito) WHERE u.email = '$email'";
+    $checkFriends = mysqli_query($connect, $checkFriends_query);
+
+    $conta_amici_query = "SELECT COUNT(*) AS nAmici FROM Utente u INNER JOIN follow f ON (f.seguente=u.email) INNER JOIN follow ff ON (ff.seguito,ff.seguente) = (f.seguente,f.seguito) INNER JOIN Utente uf ON (uf.email = f.seguito) WHERE u.email = '$email'";
+    $conta_amici = mysqli_query($connect, $conta_amici_query);
+
+    while($row = mysqli_fetch_array($conta_amici)){
+        $nAmici = mysqli_real_escape_string($connect, $row["nAmici"]);
+    }   
+
+    //istanzio array che mi servirà per splittare le righe
+    $json_array_amici = array();
+
+    //splitto le righe che ottengo dalla query
+    while ($row = mysqli_fetch_assoc($checkFriends)) {
+        $json_array_amici[] = $row;
+    }
+
+        //istanzio l'array per creare la matrice
+    $amici = array();
+
+        //ciclo che assegna per ogni array i campi
+        foreach ($json_array_amici as &$rows) {
+            array_push($amici, array(
+                'nome_amico' => $rows["nome_amico"],
+                'profilo_amico' => $rows["profilo_amico"]
+            ));
+        }
+
+    //array di array
+    $post_data = array(
+        'nome' => $username,
+        'nSeguiti' => $nSeguiti,
+        'nSeguaci' => $nSeguaci,
+        'nAmici' => $nAmici,
+        'seguiti' => $seguiti,
+        'seguaci' => $seguaci,
+        'amici' => $amici
+    );
+
+    //codifica in json
+    $output = json_encode($post_data);
+
+    echo $output;
+
+?>
