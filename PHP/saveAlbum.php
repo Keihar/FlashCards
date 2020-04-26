839de76087598f991c5e4224b66cb6e885515a52
alegordan <agorla10@gmail.com>
2020-04-26 14:34:15 +0100

    PHP saveAlbum con controllo se è già stato salvato

--- PHP/saveAlbum.php
+++ PHP/saveAlbum.php
@@ -9,25 +9,35 @@
     $id_album = mysqli_real_escape_string($connect, $_POST["id_album"]);
     $email = mysqli_real_escape_string($connect, $_POST["email"]);
     
-    $result_str = "SELECT * FROM album_salvati WHERE emailUtente = '$email' AND idAlbum = '$id_album')";
+    $result_str = "SELECT id FROM album_salvati WHERE emailUtente = '$email' AND idAlbum = '$id_album'";
     $result = mysqli_query($connect, $result_str);
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        $array[] = $row;
-    }
+    if($result){
 
-    if (mysqli_num_rows($array)==0) { exit("saveExisting"); }
+        while($row = mysqli_fetch_array($result)){
+            $salvati = mysqli_real_escape_string($connect, $row["id"]);
+        }
 
-    $insert_saved_str = "INSERT INTO album_salvati(emailUtente, idAlbum) VALUES ('$email', '$id_album')";
-    $insert_saved = mysqli_query($connect, $insert_saved_str);
+    if (mysqli_num_rows($result)!=0) { exit("saveExisting"); }
 
-    //se non esiste alcun utente con le credenziali inseritee
-    if(!$insert_saved){
-        echo "error";
-    }
     else{
-        echo "success";
+
+        $insert_saved_str = "INSERT INTO album_salvati(emailUtente, idAlbum) VALUES ('$email', '$id_album')";
+        $insert_saved = mysqli_query($connect, $insert_saved_str);
+    
+        //se non esiste alcun utente con le credenziali inseritee
+        if(!$insert_saved){
+            echo "error";
+        }
+        else{
+            echo "success";
+        }
     }
+    }
+
+
+
+
     
 
 ?>
