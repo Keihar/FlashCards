8eb03890487a88ea0dfcf49a15ab9f27eea511e7
alegordan <agorla10@gmail.com>
2020-04-27 14:29:42 +0100

    dashboardJSON e modify user con nuova password

---
+++ PHP/dashboardJSON.php
@@ -0,0 +1,72 @@
+<?php
+    //Restituisco gli ultimi 6 album creati dagli utenti che seguo (utente di sess)
+    //Restituisco gli stessi dati degli album come searchquery.php e limito a 6
+
+    include 'DBconnect.php';
+
+    session_start();
+
+
+    $user = $_SESSION["utente"];
+
+    $album_seguiti_query = "SELECT album.id AS id, album.nome AS nome, album.descrizione AS descrizione, album.imgLink AS imgLink, u.nome AS nome_seguito, u.imgProfilo AS profilo_seguito FROM Utente INNER JOIN follow f ON (f.seguente=Utente.email) INNER JOIN Utente u ON (u.email=f.seguito) INNER JOIN Album ON (album.email = u.email) WHERE Utente.nome='$user' ORDER BY data DESC LIMIT 6";
+    $album_seguiti  = mysqli_query($connect, $album_seguiti_query);
+
+    $json_album_array = array();
+
+    while ($row = mysqli_fetch_assoc($album_seguiti)) {
+        $json_album_array[] = $row;
+    }
+
+        //istanzio l'array per creare la matrice
+        $albums = array();
+
+        //ciclo che assegna per ogni array i campi
+        foreach ($json_album_array as &$rows) {
+            unset($flashcards); // $foo is gone
+            $flashcards = array(); // $foo is here again
+            unset($json_flashcard_array); // $foo is gone
+            $json_flashcard_array = array();
+
+            $id_album = $rows['id'];
+
+            $preleva_flashcard_query = "SELECT * FROM Flashcard WHERE id_album = ".$id_album."";
+            $preleva_flashcard = mysqli_query($connect, $preleva_flashcard_query);
+
+
+        while($riga = mysqli_fetch_assoc($preleva_flashcard)){
+            $json_flashcard_array[] = $riga;
+        }
+
+        foreach($json_flashcard_array as &$righe){
+            array_push($flashcards, array(
+                'id_album' => $righe['id_album'], 
+                'id_flashcard' => $righe['id'],
+                'fronte' => $righe['fronte'],
+                'retro' => $righe['retro']
+            ));
+        }
+
+            array_push($albums, array(
+                'id' => $rows['id'],
+                'nome' => $rows['nome'],
+                'descrizione' => $rows['descrizione'],
+                'imgLink' => $rows['imgLink'],
+                'username' => $rows['nome_seguito'],
+                'imgProfilo' => $rows['profilo_seguito'],
+                'flashcards' => $flashcards
+            ));
+        }
+  
+        
+    //array di array
+    $post_data = array(
+        'albums' => $albums
+    );
+
+    //codifica in json
+    $output = json_encode($post_data);
+
+    echo $output;
+
+?>
--- PHP/modifyUser.php
+++ PHP/modifyUser.php
@@ -7,21 +7,29 @@
         session_start();
 
         $user = mysqli_real_escape_string($connect, $_SESSION['utente']);
+        $password = mysqli_real_escape_string($connect, $_POST["password"]);
 
+        $check_password_query = "SELECT password FROM Utente WHERE password = '$password' AND nome = '$user'";
+        $check_password = mysqli_query($connect, $check_password_query);
 
-        while($row = mysqli_fetch_array($email_array)){
-            $email_vecchia = $row["email"];
+        if (mysqli_num_rows($check_password)==0) {
+            exit ("wrongPassword");
         }
 
-        $email = mysqli_real_escape_string($connect, $_POST["email"]);
+
+
+
         $imgProfilo = mysqli_real_escape_string($connect, $_POST["imgProfilo"]);
         $nome = mysqli_real_escape_string($connect, $_POST["nome"]);
         $motto = mysqli_real_escape_string($connect, $_POST["motto"]);
-        $password = mysqli_real_escape_string($connect, $_POST["password"]);
+
+        $new_password = mysqli_real_escape_string($connect, $_POST["newPassword"]);
+
 
         //Grazie a ON UPDATE CASCADE è possibile effettuare le modifiche in cascata anche per le altre tabelle
-        $modify_user_str = "UPDATE Utente SET email = '$email', imgProfilo='$imgProfilo', nome='$nome', motto='$motto', password='$password' WHERE nome='$user'";
+        $modify_user_str = "UPDATE Utente SET imgProfilo='$imgProfilo', nome='$nome', motto='$motto', password='$new_password' WHERE nome='$user'";
         $modify_user = mysqli_query($connect, $modify_user_str);
+
 
 
 ?>
